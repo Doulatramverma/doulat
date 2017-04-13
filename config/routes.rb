1fd7d054144a431dfd35375cc983fe3fc1ae147f
bittern <bittern@EMAIL.com>
2017-04-13 18:23:16 +0530

    inital commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :products
+  resources :categries
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  resources :blogs do 
+  	resources :comments
+  end
+  
+
+   root 'blogs#index'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
